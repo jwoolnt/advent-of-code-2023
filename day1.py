6ea61260c444dbfbccc3089de2c20986924b2137
Jayden Wool <52765228+jwoolnt@users.noreply.github.com>
2023-12-01 20:38:50 -0600

    Day 1 (Part 2)
    Both tests are correct, but it's still wrong for the actual input for some reason.

--- day1.py
+++ day1.py
@@ -1,11 +1,32 @@
 from re import findall as fa
 
+
+search = "[0-9]|one|two|three|four|five|six|seven|eight|nine"
+
+word_to_num_string = {
+	"one":"1",
+	"two":"2",
+	"three":"3",
+	"four":"4",
+	"five":"5",
+	"six":"6",
+	"seven":"7",
+	"eight":"8",
+	"nine":"9"
+}
+
+
+def search_strings_to_int(str1: str, str2: str):
+	str1 = str1 if len(str1) == 1 else word_to_num_string[str1]
+	str2 = str2 if len(str2) == 1 else word_to_num_string[str2]
+	return int(str1 + str2)
+
 def day1(input: str) -> None:
 	sum: int = 0
 
 	for line in input.splitlines():
-		num_strings: list[str] = fa("[0-9]", line)
-		sum += int(num_strings[0] + num_strings[-1])
+		search_strings: list[str] = fa(search, line)
+		sum += search_strings_to_int(search_strings[0], search_strings[-1])
 
 	print(sum)
 
@@ -14,6 +35,14 @@ day1("""1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet""")
+
+day1("""two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen""")
 
 day1("""xt36five77
 two8five6zfrtjj
