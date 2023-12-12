48aa86e55a8070044eab460a5e04b7796bc92a71
Jayden Wool <52765228+jwoolnt@users.noreply.github.com>
2023-12-12 15:29:31 -0500

    Updated Solution 2023-2 for CLI

--- year2023/day2.py
+++ year2023/day2.py
@@ -1,18 +1,10 @@
-cube_amounts = {
-	"red": 12,
-	"green": 13,
-	"blue": 14
-}
-
-
-def day2(input: str) -> None:
+def setup(input: str) -> dict[str, dict[str, int]]:
 	data: dict[str, dict[str, int]] = {}
 
 	for game in input.splitlines():
 		[game_id, game_data] = game.split(":")
-		num_string_id = game_id.split(" ")[-1]
-
-		data[num_string_id] = {
+		game_num = game_id.split(" ")[-1]
+		data[game_num] = {
 			"red": 0,
 			"green": 0,
 			"blue": 0
@@ -23,36 +15,46 @@ def day2(input: str) -> None:
 				[*_, num, color] = datapoint.split(" ")
 				num = int(num)
 
-				if data[num_string_id][color] < num:
-					data[num_string_id][color] = num
+				if data[game_num][color] < num:
+					data[game_num][color] = num
+
+	return data
+
+cube_amounts = {
+	"red": 12,
+	"green": 13,
+	"blue": 14
+}
 
 
-	# Part 1
-	sum = 0
+def part1(input: str):
+	data: dict[str, dict[str, int]] = setup(input)
+	sum: int = 0
 
-	for game_id in data:
-		possible = True
+	for game_num in data:
+		possible: bool = True
 
-		for color in data[game_id]:
-			if cube_amounts[color] < data[game_id][color]:
+		for color in data[game_num]:
+			if cube_amounts[color] < data[game_num][color]:
 				possible = False
 				break
 
 		if possible:
-			sum += int(game_id)
+			sum += int(game_num)
 
-	print("Part 1:", sum)
+	return sum
 
 
-	# Part 2
-	sum = 0
+def part2(input: str):
+	data: dict[str, dict[str, int]] = setup(input)
+	sum: int = 0
 
-	for game_id in data:
-		power = 1
+	for game_num in data:
+		power: int = 1
 
-		for color in data[game_id]:
-			power *= data[game_id][color]
+		for color in data[game_num]:
+			power *= data[game_num][color]
 
 		sum += power
 
-	print("Part 2:", sum)
+	return sum
