9fa98fc0e733ceb1e412afa008ab86872b26ea88
Jayden Wool <52765228+jwoolnt@users.noreply.github.com>
2023-12-11 23:55:16 -0500

    Added Testing Year

---
+++ year0000/day0.py
@@ -0,0 +1,11 @@
+def part1(input: str):
+	print(input)
+
+def part2(input: str):
+	print(reversed(input))
+
+inputs = {
+	part1: "This is part1 IN-FILE test input!",
+	part2: "PART2 HAS AN IN-FILE INPUT AS WELL??",
+	"*": "This in-file should run for both"
+}
