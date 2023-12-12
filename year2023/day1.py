7e4eb7ef85979daa35690a881f967934bb0fa846
Jayden Wool <52765228+jwoolnt@users.noreply.github.com>
2023-12-12 15:03:30 -0500

    Updated Solution 2023-1 for CLI

--- year2023/day1.py
+++ year2023/day1.py
@@ -1,20 +1,5 @@
-word_to_num_string = {
-	"one":"1",
-	"two":"2",
-	"three":"3",
-	"four":"4",
-	"five":"5",
-	"six":"6",
-	"seven":"7",
-	"eight":"8",
-	"nine":"9"
-}
-
-search = list(word_to_num_string.keys())
-search.extend(word_to_num_string.values())
-
-
-def day1(input: str) -> None:
+def part1(input: str) -> int:
+	search = "123456789"
 	sum: int = 0
 
 	for line in input.splitlines():
@@ -24,9 +9,37 @@ def day1(input: str) -> None:
 		first = first_dict[min(first_dict.keys())]
 		last = last_dict[max(last_dict.keys())]
 
-		first = first if first in word_to_num_string.values() else word_to_num_string[first]
-		last = last if last in word_to_num_string.values() else word_to_num_string[last]
+		sum += int(first + last)
+
+	return sum
+
+
+def part2(input: str) -> int:
+	word_to_num_string = {
+		"one":"1",
+		"two":"2",
+		"three":"3",
+		"four":"4",
+		"five":"5",
+		"six":"6",
+		"seven":"7",
+		"eight":"8",
+		"nine":"9"
+	}
+	search = list(word_to_num_string.keys())
+	search.extend(word_to_num_string.values())
+	sum: int = 0
+
+	for line in input.splitlines():
+		first_dict = {line.find(item): item for item in search if line.find(item) != -1}
+		last_dict = {line.rfind(item): item for item in search if line.rfind(item) != -1}
+
+		first = first_dict[min(first_dict.keys())]
+		last = last_dict[max(last_dict.keys())]
+
+		first = word_to_num_string[first] if first in word_to_num_string else first
+		last = word_to_num_string[last] if last in word_to_num_string else last
 
 		sum += int(first + last)
 
-	print(sum)
+	return sum
